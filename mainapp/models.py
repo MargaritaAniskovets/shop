0a64a2ffb312a820ccf50682dfebb4d4819a1a0f
Margarita <rita.aniskovet@gmail.com>
2020-12-11 23:48:00 +0300

    add model registration to admin and statics to settings

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,19 +1,15 @@
 from django.conf import settings
+from django.contrib.contenttypes import fields
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 User = get_user_model()
 settings.AUTH_USER_MODEL
 
-# Catrgory
-# Product
-# CartProduct
-# Cart
-# Order
-# ***
-# Customer
-# Specification
 class Category(models.Model):
+
     name = models.CharField(max_length=255, verbose_name='Название категории')
     slug = models.SlugField(unique=True)
 
@@ -21,49 +17,83 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-    category = models.ForeignKey(Category,verbose_name='Категория',on_delete=models.CASCADE)
+
+    # class Meta:
+    #     abstract = True
+
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование продукта')
     slug = models.SlugField(unique=True)
     image = models.ImageField(verbose_name='Изображение')
     description = models.TextField(verbose_name='Описание', null=True)
-    price = models.DecimalField(max_digits=9, decimal_places=2,verbose_name='Цена')
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
 
     def __str__(self):
         return self.title
 
+class Notebook(Product):
+    diagonal = models.CharField(max_length=255,verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    processor_freg = models.CharField(max_length=255, verbose_name='Частота процессора')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    video = models.CharField(max_length=255, verbose_name='Видеокамера')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Время работы аккумулятора')
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+
+class Smartphone(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    resolution = models.CharField(max_length=255, verbose_name='Разрешение экрана')
+    accum_volume = models.CharField(max_length=255, verbose_name='Объем батареи')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    sd = models.BooleanField(default=True)
+    sd_volume_max = models.CharField(max_length=255, verbose_name='Максимальный объем оперативной памяти')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Главная камера')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='Фронтальная камера')
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+
 class CartProduct(models.Model):
-    user = models.ForeignKey('Customer',verbose_name='Покупатель',on_delete=models.CASCADE)
-    cart = models.ForeignKey('Cart',verbose_name='Корзина',on_delete=models.CASCADE)
-    product = models.ForeignKey(Product,verbose_name='Товар',on_delete=models.CASCADE)
+
+    user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField()
+    content_object = fields.GenericForeignKey('content_type', 'object_id')
     qty = models.PositiveBigIntegerField(default=1)
-    final_price = models.DecimalField(max_digits=9, decimal_places=2,verbose_name='Общая цена')
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
     def __str__(self):
         return 'Продукт: {}(для корзины)'.format(self.product.title)
 
 class Cart(models.Model):
-    owner = models.ForeignKey('Customer',verbose_name='Владелец',on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product,blank=True)
+
+    owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, blank=True, related_name='related_cart')
     total_products = models.PositiveBigIntegerField(default=0)
-    final_price = models.DecimalField(max_digits=9, decimal_places=2,verbose_name='Общая цена')
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
     def __str__(self):
         return str(self.id)
 
 class Customer(models.Model):
-    user = models.ForeignKey(User,verbose_name='Пользователь',on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20,verbose_name='Номер телефона')
-    address = models.CharField(max_length=255,verbose_name='Адрес')
+
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, verbose_name='Номер телефона')
+    address = models.CharField(max_length=255, verbose_name='Адрес')
 
     def __str__(self):
         return 'Покупатель: {}{}'.format(self.user.first_name,self.user.last_name)
 
-class Specifications(models.Model):
-
-    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
-    object_id = models.PositiveBigIntegerField()
-    name = models.CharField(max_length=255,verbose_name='Имя товара для характеристик')
-
-    def __str__(self):
-        return 'Характеристики для товара: {}{}'.format(self.name)
+# class Specifications(models.Model):
+#
+#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+#     object_id = models.PositiveBigIntegerField()
+#     name = models.CharField(max_length=255, verbose_name='Имя товара для характеристик')
+#
+#     def __str__(self):
+#         return 'Характеристики для товара: {}{}'.format(self.name)
 
