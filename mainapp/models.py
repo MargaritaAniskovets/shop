58d0f77ef306c3a435d1537dd745d238b86e707b
Margarita <rita.aniskovet@gmail.com>
2020-12-14 00:04:09 +0300

    image upload restrictions

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django.conf import settings
 from django.contrib.contenttypes import fields
 from django.db import models
@@ -8,6 +9,12 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 User = get_user_model()
 settings.AUTH_USER_MODEL
 
+
+class MinResolutionErrorException(Exception):
+    pass
+
+class MaxResolutionErrorException(Exception):
+    pass
 
 class LatestProductsManager:
 
@@ -43,9 +50,13 @@ class Category(models.Model):
 
 class Product(models.Model):
 
+    MIN_RESOLUTION = (300, 300)
+    MAX_RESOLUTION = (800, 800)
+    MAX_IMAGE_SIZE = 3145728
+
     class Meta:
         pass
-    #     abstract = True
+        # abstract = True
 
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование продукта')
@@ -56,6 +67,18 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args,**kwargs):
+        image = self.image
+        img = Image.open(image)
+        min_height, min_width = self.MIN_RESOLUTION
+        max_height, max_width = self.MAX_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise MinResolutionErrorException('Разрешение изображения меньше минимального')
+        if img.height > max_height or img.width > max_width:
+            raise MaxResolutionErrorException('Разрешение изображения больше максимального')
+        return image
+
 
 class Notebook(Product):
     diagonal = models.CharField(max_length=255,verbose_name='Диагональ')
