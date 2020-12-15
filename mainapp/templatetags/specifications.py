ef4faeccfda0081848ea847282c0bc9bc02118b3
Margarita <rita.aniskovet@gmail.com>
2020-12-16 00:29:49 +0300

    Custom template tags. Loading characteristics through a filter

---
+++ mainapp/templatetags/specifications.py
@@ -0,0 +1,51 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+TABLE_HEAD = '''
+                <table class="table">
+                  <tbody>
+            '''
+
+TABLE_TAIL = '''
+                 </tbody>
+                </table>
+            '''
+TABLE_CONTENT = '''
+                    <tr>
+                      <td>{name}</td>
+                      <td>{value}</td>
+                    </tr>
+                '''
+PPRODUCT_SPEC = {
+    'notebook': {
+        'Диагональ': 'diagonal',
+        'Тип дисплея': 'display_type',
+        'Частота процессора': 'processor_freg',
+        'Оперативная память': 'ram',
+        'Видеокамера': 'video',
+        'Время работы аккумулятора': 'time_without_charge ',
+    },
+    'smartphone': {
+        'Диагональ': 'diagonal',
+        'Тип дисплея': 'display_type',
+        'Разрешение экрана': 'resolution',
+        'Объем батареи': 'accum_volume',
+        'Оперативная память': 'ram',
+        'Максимальный объем оперативной памяти': 'sd_volume_max',
+        'Камера (МП)': 'main_cam_mp',
+        'Фронтальная камера (МП)': 'frontal_cam_mp',
+        'Наличие слота для cd карты': 'sd'
+    }
+}
+def get_product_spec(product, model_name):
+    table_content = ''
+    for name, value in PPRODUCT_SPEC[model_name].items():
+        table_content += TABLE_CONTENT.format(name=name, value=getattr(product, value))
+    return table_content
+
+@register.filter
+def product_spec(product):
+    model_name = product.__class__._meta.model_name
+    return mark_safe( TABLE_HEAD + get_product_spec(product, model_name) + TABLE_TAIL)
